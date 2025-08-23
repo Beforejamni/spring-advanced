f5c8bef837f15268fe1c28c590c3c4f6379d4855
Beforejamni <beforejamn1@gmail.com>
2025-08-23 10:05:10 +0900

    level1-3 : Validation

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -1,9 +1,12 @@
 package org.example.expert.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 
 @Getter
 @NoArgsConstructor
@@ -13,5 +16,8 @@ public class UserChangePasswordRequest {
     @NotBlank
     private String oldPassword;
     @NotBlank
+    @Pattern(regexp = ".*\\d.*", message = "숫자를 포함해야 합니다.")
+    @Pattern(regexp = ".*[A-Z].*", message =  "대문자를 포함해야 합니다.")
+    @Size(min = 8, message ="새 비밀번호는 8자 이상이어야 합니다" )
     private String newPassword;
 }
